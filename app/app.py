3c62528ca50e762458860992b2c3727c27a90bbe
Hammad Ali <49644704+hammad11ali@users.noreply.github.com>
2019-11-30 17:52:10 +0500

    Initial Commit

---
+++ app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+@app.route('/')
+def home():
+    return render_template('home.html')
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+def knn():
+    re=[]
+    return re
---
+++ app/csvData.py
@@ -0,0 +1,6 @@
+import pandas
+result = pandas.read_csv('MetaData.csv')
+
+df1 = result.values.tolist()[3][0]
+print(df1)
+    
